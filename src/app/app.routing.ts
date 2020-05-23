7ab534264e1820b77c74edd1df17d957126cb03f
Laso de la Vega <r.laso.de.la.vega@accenture.com>
2020-05-22 21:17:47 -0300

    login

---
+++ src/app/app.routing.ts
@@ -0,0 +1,10 @@
+import { RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+
+
+const appRoutes = [
+  { path: "", component: AppComponent, pathMatch: "full" },
+  { path: "login", component: LoginComponent, pathMatch: "full" },
+];
+export const routing = RouterModule.forRoot(appRoutes);
